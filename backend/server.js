930bf9624a2e62d16052403e1a46956821901bf9
Shriharsh <shriharshgautam7@gmail.com>
2024-03-05 14:22:24 +0530

    created routes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv';
+import authRoutes from './routes/auth.routes.js';
 
 const app = express();
 dotenv.config();
@@ -11,18 +12,7 @@ app.get('/', (req,res) => {
     res.send("Hello World! how are you doing");
 })
 
-app.get('/api/auth/signup', (req,res) => {
-    console.log('signup route');
-})
-
-app.get('api/auth/login', (req,res) => {
-    console.log('login route');
-})
-
-app.get('/api/auth/logout', (req,res) => {
-    console.log('logout route');
-})
-
+app.use('/api/auth', authRoutes)
 
 app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
 
