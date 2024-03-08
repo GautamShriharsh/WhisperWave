de928867e4db29f1498634076872667e01e0daa9
Shriharsh <shriharshgautam7@gmail.com>
2024-03-08 20:28:34 +0530

    Created signup function

--- backend/server.js
+++ backend/server.js
@@ -4,16 +4,20 @@ import connectToMongoDB from './db/connectToMongoDB.js'
 import authRoutes from './routes/auth.routes.js';
 
 const app = express();
-dotenv.config();
 const PORT = process.env.PORT || 5000;
 
+dotenv.config();
 
-app.get('/', (req,res) => {
-    //root route http://localhost:5000/
-    res.send("Hello World! how are you doing");
-})
+app.use(express.json()); // Middleware for parsing JSON payload from req.body
 
 app.use('/api/auth', authRoutes)
+
+
+// app.get('/', (req,res) => {
+//     //root route http://localhost:5000/
+//     res.send("Hello World! how are you doing");
+// })
+
 
 app.listen(PORT, () => {
     connectToMongoDB();
