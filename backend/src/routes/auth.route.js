f33a35c485817257df064c97008398dc04df2546
Shriharsh <shriharshgautam7@gmail.com>
2024-12-26 13:25:11 +0530

    created mongodb connection

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -3,10 +3,10 @@ import {signup,login,logout} from "../controllers/auth.controller.js"
 
 const router = express.Router();
 
-router.get("/signup",signup ) 
+router.get("/signup",signup) 
 
 
-router.get("/login", login)
+router.get("/login",login)
 
 
 router.get("/logout",logout)
